4fc026a6918775b5c5cb6eeb999222aa70e694fd
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2024-08-07 18:04:39 +0300

    grafana install

--- Docker_lesson/app/src/server.py
+++ Docker_lesson/app/src/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 import psycopg2
+from metrics import register_metrics
 
 app = Flask(__name__)
-
 
 def get_db_connection():
     conn = psycopg2.connect(host='postgres',
@@ -14,6 +14,23 @@ def get_db_connection():
 @app.route("/health")
 def hello():
     return {"status": "OKEY"}
+
+@app.route("/createDB")
+def createDB():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS users;')
+    cur.execute('CREATE TABLE users (id serial PRIMARY KEY,'
+                'username varchar (256) NOT NULL,'
+                'firstName varchar (50) NOT NULL,'
+                'lastName varchar (50) NOT NULL,'
+                'email varchar (50) NOT NULL,'
+                'phone varchar (50) NOT NULL);'
+                )
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -93,6 +110,13 @@ def user(user_id):
         conn.close()
         return {"user deleted": "OK"}
 
+
+@app.route('/metrics')
+def metrics():
+    from prometheus_client import generate_latest
+    return generate_latest()
+
 # run the application
 if __name__ == "__main__":
+    register_metrics(app)
     app.run(host='0.0.0.0', debug=True, port=8000)
